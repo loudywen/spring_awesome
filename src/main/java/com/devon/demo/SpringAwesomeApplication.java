fa3530f034ebcd85a39f2ab007b95de16077b7b7
Devon <loudywen@gmail.com>
2016-12-26 06:35:40 -0600

    Initial commit

---
+++ src/main/java/com/devon/demo/SpringAwesomeApplication.java
@@ -0,0 +1,13 @@
+package com.devon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringAwesomeApplication {
+
+    public static void main(String[] args) {
+    SpringApplication.run(SpringAwesomeApplication.class, args);
+  }
+}
