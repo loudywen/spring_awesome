bb44e03e4c6305057d0e672f55cffbab39e8caa1
Devon <loudywen@gmail.com>
2017-01-07 21:59:13 -0600

    added spark streaming for kafka

--- src/main/java/com/devon/demo/controller/KafkaRestController.java
+++ src/main/java/com/devon/demo/controller/KafkaRestController.java
@@ -1,10 +1,12 @@
 package com.devon.demo.controller;
 
 import com.devon.demo.kafka.config.KafkaConfig;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,25 +16,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class KafkaRestController {
 	@Autowired
 	private KafkaTemplate<Integer, String> kt;
-
+	static final Logger        LOG   = LoggerFactory.getLogger(KafkaRestController.class);
+	private      AtomicInteger count = new AtomicInteger();
+private String tempStr ="{\"mem\":2146596,\"mem.free\":721702,\"processors\":16,\"instance.uptime\":85339,\"uptime\":93719,\"systemload.average\":-1.0,\"heap.committed\":2048512,\"heap.init\":524288,\"heap.used\":1326809,\"heap\":7431168,\"nonheap.committed\":99392,\"nonheap.init\":2496,\"nonheap.used\":98097,\"nonheap\":0,\"threads.peak\":40,\"threads.daemon\":18,\"threads.totalStarted\":48,\"threads\":40,\"classes\":11296,\"classes.loaded\":11296,\"classes.unloaded\":0,\"gc.ps_scavenge.count\":15,\"gc.ps_scavenge.time\":202,\"gc.ps_marksweep.count\":3,\"gc.ps_marksweep.time\":243,\"jvm.memory-usage.pools.Code-Cache.max\":251658240,\"com.devon.demo.kafka.config.Listener.listener_call.fiveMinuteRate\":44628.487552979816,\"jvm.memory-usage.pools.PS-Survivor-Space.max\":79167488,\"jvm.memory-usage.pools.PS-Old-Gen.init\":358088704,\"jvm.threads.new.count\":0,\"jvm.gc.PS-Scavenge.time\":202,\"jvm.threads.daemon.count\":18,\"jvm.memory-usage.heap.max\":7609516032,\"jvm.memory-usage.pools.PS-Eden-Space.committed\":1558183936,\"jvm.memory-usage.pools.Metaspace.max\":-1,\"jvm.threads.blocked.count\":0,\"jvm.memory-usage.pools.PS-Old-Gen.max\":5707399168}";
 
 	@PostMapping("/produce/{str}")
 	public void produce(@PathVariable("str") String str) {
-        sendBrust(str);
-    }
+		kt.send("devontest2", str);
+	}
 
-	@PostMapping("/producebrust/{str}")
-	public void producebrust(@PathVariable("str") String str) {
-        sendBrust(str);
-    }
+	@PostMapping("/producebrust/{str}/{load}")
+	public void producebrust(@PathVariable("str") String str, @PathVariable("load") String load) {
+		int temp = Integer.parseInt(load);
+		for (int x = 0; x < temp; x++) {
+			kt.send("devontest2", count.incrementAndGet() + " - "+ str);
+		}
+	}
 
-    @Async
-    private void sendBrust(@PathVariable("str") String str) {
-	    for(int x = 0; x<1000000; x++){
-            kt.send("devontest", str);
-
-        }
-    }
+	// @Async
+	// private void sendBrust(@PathVariable("str") String str) {
+	// for (int x = 0; x < 1000000; x++) {
+	// kt.send("devontest", str);
+	//
+	// }
+	// }
 
 
 
--- src/main/java/com/devon/demo/kafka/config/KafkaConfig.java
+++ src/main/java/com/devon/demo/kafka/config/KafkaConfig.java
@@ -1,26 +1,39 @@
 package com.devon.demo.kafka.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 @EnableKafka
 @Conditional(KafkaConfig.class)
 public class KafkaConfig implements Condition {
+
+	@Autowired
+	private Environment env;
+
 	@Bean
 	ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory() {
-		ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
-				new ConcurrentKafkaListenerContainerFactory<>();
+		ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory());
-		factory.setConcurrency(10);
+		factory.setConcurrency(40);
 		return factory;
 	}
 
@@ -32,15 +45,13 @@ public class KafkaConfig implements Condition {
 	@Bean
 	public Map<String, Object> consumerConfigs() {
 		Map<String, Object> props = new HashMap<>();
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.16.143.137:9092");
-		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-				org.apache.kafka.common.serialization.IntegerDeserializer.class);
-		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-				org.apache.kafka.common.serialization.StringDeserializer.class);
-		props.put(ConsumerConfig.GROUP_ID_CONFIG, "devon_kafka_consumer");
-		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
-		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("kafka.bootstrap.servers"));
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.IntegerDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringDeserializer.class);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, env.getProperty("kafka.consumer.group.id"));
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, Boolean.getBoolean(env.getProperty("kafka.enable.auto.commit")));
+		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, env.getProperty("kafka.auto.commit.interval.ms"));
+		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, env.getProperty("kafka.session.timeout.ms"));
 		return props;
 	}
 
@@ -57,18 +68,14 @@ public class KafkaConfig implements Condition {
 	@Bean
 	public Map<String, Object> producerConfigs() {
 		Map<String, Object> props = new HashMap<>();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.16.143.137:9092");
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-				org.apache.kafka.common.serialization.IntegerSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-				org.apache.kafka.common.serialization.StringSerializer.class);
-		props.put(ProducerConfig.RETRIES_CONFIG, 0);
-		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-		props.put(ProducerConfig.CLIENT_ID_CONFIG, "devon_kafka_producer");
-
-
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("kafka.bootstrap.servers"));
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.IntegerSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+		props.put(ProducerConfig.RETRIES_CONFIG, Integer.parseInt(env.getProperty("kafka.retries")));
+		props.put(ProducerConfig.BATCH_SIZE_CONFIG, Integer.parseInt(env.getProperty("kafka.batch.size")));
+		props.put(ProducerConfig.LINGER_MS_CONFIG, Integer.parseInt(env.getProperty("kafka.linger.ms")));
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, Integer.parseInt(env.getProperty("kafka.buffer.memory")));
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, env.getProperty("kafka.producer.client.id"));
 		return props;
 	}
 
@@ -79,7 +86,6 @@ public class KafkaConfig implements Condition {
 
 	@Override
 	public boolean matches(ConditionContext arg0, AnnotatedTypeMetadata arg1) {
-		// TODO Auto-generated method stub
 		return true;
 	}
 }
